8a373bbe154f2caadead71381099622b7451f769
Ayan shaikh <44862892+ayan8125@users.noreply.github.com>
2025-12-07 17:31:40 +0530

    Add security, ALB, and ASG modules to main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,33 @@ module "subnets" {
 
 }
 
+module "security" {
+  source       = "./modules/security"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+}
+
+module "alb" {
+    source = "./modules/alb"
+    vpc_id = module.vpc.vpc_id
+    project_name = var.project_name
+    alb_security_group_id = module.security.alb_sg_id
+    public_subnet_ids = module.subnets.public_subnet_ids
+}
+
+module "asg" {
+  source = "./modules/asg"
+
+  project_name          = var.project_name
+  vpc_id                = module.vpc.vpc_id
+  private_subnet_ids    = module.subnets.private_subnet_ids
+  ec2_security_group_id = module.security.ec2_sg_id
+  target_group_arn      = module.alb.target_group_arn
+  instances_type = var.instances_types
+  ami_id = var.ami_id
+  ec2_profile_name = module.iamroles.ec2_profile_name
+}
+
 
 output "vpc_id" {
     value = module.vpc.vpc_id
@@ -42,4 +69,9 @@ output "aws_nat_gateway" {
 
 output "nat_elastic_ip" {
     value = module.subnets.aws_eip_ids
+}
+
+
+output "alb_dns" {
+  value = module.alb.alb_dns_name
 }
