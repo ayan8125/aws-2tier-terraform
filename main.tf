057f2112b4a4e6e3b94c0c44de52b1679433d4b0
Ayan shaikh <44862892+ayan8125@users.noreply.github.com>
2025-12-07 16:41:41 +0530

    Add subnets module to main Terraform configuration

--- main.tf
+++ main.tf
@@ -3,3 +3,17 @@ module "vpc" {
   project_name = var.project_name
   cidr_block   = "10.0.0.0/16"
 }
+
+
+module "subnets" {
+    source = "./modules/subnets"
+    vpc_id = module.vpc.vpc_id
+    project_name = var.project_name
+    region = var.aws_region
+    
+    azs = [
+        "us-east-1a",
+        "us-east-1b"
+    ]
+
+}
